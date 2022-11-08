3776199bc048230b4695a49f9480c6704187c50c
Your Name <you@example.com>
2022-11-08 18:26:25 +0100

    singel service details added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -24,6 +24,21 @@ async function run() {
           const services = await cursor.toArray();
           res.send(services);
       });
+      app.get('/servicess', async (req, res) => {
+        const query = {}
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+  });
+
+
     }
   finally {
 
